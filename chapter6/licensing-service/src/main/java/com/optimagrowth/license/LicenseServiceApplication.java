cd24823c73b2b70bf80d364545a32c542bd2ee17
AlexYushchenko <yualeksandr@rambler.ru>
2025-05-05 18:54:50 +0300

    chapter6: refactor

---
+++ chapter6/licensing-service/src/main/java/com/optimagrowth/license/LicenseServiceApplication.java
@@ -0,0 +1,46 @@
+package com.optimagrowth.license;
+
+import java.util.Locale;
+
+import com.optimagrowth.license.config.ServiceConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@SpringBootApplication
+@RefreshScope
+@EnableConfigurationProperties(ServiceConfig.class)
+@EnableDiscoveryClient
+@EnableFeignClients
+public class LicenseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicenseServiceApplication.class, args);
+	}
+
+	@Bean
+	public LocaleResolver myResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setUseCodeAsDefaultMessage(true);
+		messageSource.setBasenames("messages");
+		return messageSource;
+	}
+
+	@LoadBalanced
+	@Bean
+	public RestTemplate getRestTemplate(){
+		return new RestTemplate();
+	}
+
+}
---
+++ chapter6/licensing-service/src/main/java/com/optimagrowth/license/controller/LicenseController.java
@@ -0,0 +1,49 @@
+package com.optimagrowth.license.controller;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.optimagrowth.license.model.License;
+import com.optimagrowth.license.service.LicenseService;
+
+@RestController
+@RequestMapping(value="v1/organization/{organizationId}/license")
+@RequiredArgsConstructor
+public class LicenseController {
+
+	private final LicenseService licenseService;
+
+	@GetMapping(value="/{licenseId}")
+	public ResponseEntity<License> getLicense(@PathVariable("organizationId") String organizationId,
+											  @PathVariable("licenseId") String licenseId) {
+		var license = licenseService.getLicense(licenseId, organizationId);
+		license.add( 
+				linkTo(methodOn(LicenseController.class).getLicense(organizationId, license.getLicenseId())).withSelfRel(),
+				linkTo(methodOn(LicenseController.class).createLicense(license)).withRel("createLicense"),
+				linkTo(methodOn(LicenseController.class).updateLicense(license)).withRel("updateLicense"),
+				linkTo(methodOn(LicenseController.class).deleteLicense(license.getLicenseId())).withRel("deleteLicense")
+		);
+		
+		return ResponseEntity.ok(license);
+	}
+
+	@PutMapping
+	public ResponseEntity<License> updateLicense(@RequestBody License request) {
+		return ResponseEntity.ok(licenseService.updateLicense(request));
+	}
+	
+	@PostMapping
+	public ResponseEntity<License> createLicense(@RequestBody License request) {
+		return ResponseEntity.ok(licenseService.createLicense(request));
+	}
+
+	@DeleteMapping(value="/{licenseId}")
+	public ResponseEntity<String> deleteLicense(@PathVariable("licenseId") String licenseId) {
+		return ResponseEntity.ok(licenseService.deleteLicense(licenseId));
+	}
+}
